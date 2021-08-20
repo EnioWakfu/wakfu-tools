e8ca2f90bcef8e4fa2a84c403f7c1c2cb48200e7
EnioWakfu <dontstalkme@now.com>
2021-08-20 21:59:56 +0200

    config (pages) : add public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/wakfu-tools/' : '/',
   outputDir: path.resolve(__dirname, "docs"),
   transpileDependencies: [
     'vuetify'
