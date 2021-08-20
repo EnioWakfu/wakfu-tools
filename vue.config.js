15dd14e306930890f3f8e09a3201cdef3aedf92a
EnioWakfu <dontstalkme@now.com>
2021-08-20 22:10:37 +0200

    config (pages) : bundle in less files

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
 const path = require("path");
 
 module.exports = {
+  css: {
+    extract: false,
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: false
+    },
+  },
   publicPath: process.env.NODE_ENV === 'production' ? '/wakfu-tools/' : '/',
   outputDir: path.resolve(__dirname, "docs"),
   transpileDependencies: [
